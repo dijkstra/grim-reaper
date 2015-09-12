b1ba6affb9293916daad3c923fd0dd39cd4dd653
Erik Falck Bohlmark <erik.bohlmark@accedo.tv>
2015-09-12 16:53:16 +0200

    Fuck im tired.

---
+++ buyerapp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shitForSaleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shitForSaleApp
+ */
+angular.module('shitForSaleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
