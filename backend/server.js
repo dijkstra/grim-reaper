4974fac14a60c74de6bde5aafceaa236cec73de3
wallenius <wallenius@wallenius-macbookair.roam.corp.google.com>
2015-09-13 08:09:06 +0200

    Commit

--- backend/server.js
+++ backend/server.js
@@ -108,20 +108,23 @@ function updateItemById(id, fields) {
   return deferred.promise;
 }
 
-function reductQuanitity() {
+function reductQuanitity(itemData) {
+
+  console.log('reductQuanitity');
+  console.log(itemData);
 
   var deferred = $q.defer();
-  Item.findById(req.body.id, function(err, item) {
+  Item.findById(itemData.id, function(err, item) {
     if (err) {
-      res.send(err);
-    } if (!item) {
-
+      throw err;
+    } 
+    if (!item) {
       res.status(404).send('Item not found');
-    } else if (item.amount < req.body.quantity) {
-
-      res.status(404)
-         .send('Out of stock');
-    }
+      return;
+    } else if (item.amount < itemData.quantity) {
+      res.status(404).send('Out of stock');
+      return;
+    } 
 
     var newAmount = item.amount - req.body.quantity;
     updateItemById(item._id, { 'amount': newAmount })
@@ -136,9 +139,11 @@ router.route('/checkout')
 .post(function(req, res) {
 
   makeTransaction(req.body)
-  // .then(reductQuanitity)
+  .then(function () {
+    return reductQuanitity(req.body);
+  })
   .then(function() {
-    res.redirect('success.html');
+    res.redirect('http://0.0.0.0:9000/#/success');
     // res.send();
   }).fail(res.send);
 
